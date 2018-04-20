9a725492a99f89b911ef2b608235aee1f69a0718
Mr.Phobia <tossarporn@gmail.com>
2018-04-21 03:05:19 +0700

    join table register and customerforrent success

--- CustomerForRent.php
+++ CustomerForRent.php
@@ -5,6 +5,7 @@ $data = file_get_contents("php://input");
 $_POST = json_decode($data,TRUE);
 $details = [];
     if(isset($_POST['ref_tec']) && isset($_POST['ref_regis'])){
+            // $id = $_POST['id'];
             $name_guest = $_POST['name_guest'];
             $last_name_guest = $_POST['last_name_guest'];
             $equipment = $_POST['equipment'];
@@ -16,8 +17,7 @@ $details = [];
             $tel = $_POST['tel'];
             $ref_tec = $_POST['ref_tec'];
             $ref_regis = $_POST['ref_regis'];}
-
-        $select="SELECT * FROM `customerforrent` WHERE `ref_tec`='{$ref_tec}' ";
+        $select="SELECT * FROM `customerforrent` INNER JOIN register ON customerforrent.ref_regis = register.id WHERE `ref_tec`='{$ref_tec}'";
         if($res = mysqli_query($connection,$select)){
             if (mysqli_num_rows($res)>0) {
                 $details['message'] = "ไม่สามารถทำการจองได้";
@@ -25,8 +25,8 @@ $details = [];
         }
 
             else{
-            $insert = "INSERT INTO `customerforrent` (`id`, `name`, `lastname`,`equipment` ,`house_number`, `street`, `distric`, `area`, `tel`, `date of service`, `ref_tec`, `ref_regis`) 
-            VALUES (NULL, '{$name_guest}', '{$last_name_guest}','{$equipment}', '{$num_house}', '{$street}', '{$dristric}', '{$area}', '{$tel}', '{$date}', '{$ref_tec}', '{$ref_regis}');";
+                $insert = "INSERT INTO `customerforrent` (`id`, `name`, `lastname`,`equipment` ,`house_number`, `street`, `distric`, `area`, `tel`, `date of service`, `ref_tec`, `ref_regis`) 
+                VALUES (NULL,'{$name_guest}', '{$last_name_guest}','{$equipment}', '{$num_house}', '{$street}', '{$dristric}', '{$area}', '{$tel}', '{$date}', '{$ref_tec}', '{$ref_regis}');";
             $qury_insert = mysqli_query($connection,$insert);
             $details['message'] = "จองสำเร็จ";
             $details['status'] = true;
