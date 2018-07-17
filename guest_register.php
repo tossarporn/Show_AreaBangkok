1b1148ca5a8ee8f47fe1afc611628da0dfb6718e
Mr.Phobia <tossarporn@gmail.com>
2018-07-17 18:38:12 +0700

    create check login  guest with tecnician success

--- guest_register.php
+++ guest_register.php
@@ -15,22 +15,22 @@ if(isset($_POST['guest_name'])){
     $guest_area = $_POST['guest_area'];
     $ref_guest = $_POST['ref_guest'];
 
+    $user = $_POST['user'];
     $password = $_POST['password'];
     $status = $_POST['status'];
-    $user = $_POST['user'];
 }
 
-$select = "SELECT * FROM `register` WHERE `user`='{$user}' AND `password` ='{$password}'";
+$select = "SELECT * FROM `guest_register` WHERE `user`='{$user}' AND `password` ='{$password}'";
 if($res = mysqli_query($connection,$select)){
     if (mysqli_num_rows($res)>0) {
         $detail['message'] = "มีผู้ใช้รหัสผ่านนี้แล้ว";
         $detail['status'] = false;
     }
     else{
-        $insert_guest_password ="INSERT INTO 
-        `register` (`id`, `user`, `password`, `status`) 
-        VALUES (NULL, '{$user}', '{$password}', '$status') ";//$insert_password
-        $query_insert = mysqli_query($connection,$insert_guest_password);
+        // $insert_guest_password ="INSERT INTO 
+        // `register` (`id`, `user`, `password`, `status`) 
+        // VALUES (NULL, '{$user}', '{$password}', '$status') ";//$insert_password
+        // $query_insert = mysqli_query($connection,$insert_guest_password);
         $insert_guest_details = "INSERT INTO 
         `guest_register`(`id`, `guest_name`, `guest_lastname`, `guest_tel`, `guest_num_house`, `guest_street`, `guest_distric`, `guest_area`,`ref_guest`) 
         VALUES (null,'{$guest_name}','{$guest_lastname}','{$guest_tel}','{$guest_num_house}','{$guest_street}','{$guest_distric}','{$guest_area}','{$ref_guest}')";
